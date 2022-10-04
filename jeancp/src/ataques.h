8360e59c08f622af87c358d579b3ac13254e8443
camilamaiadevalmeida <camila.maia.almeida@uel.br>
2022-10-04 14:45:45 -0300

    ataques.h

---
+++ jeancp/src/ataques.h
@@ -0,0 +1,17 @@
+#ifndef ATAQUES_H
+#define ATAQUES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+typedef void* Ataque1;
+/* 
+
+*/
+typedef void* Ataque2;
+typedef void* Ataque3;
+
+/*Um ataque é uma estrutura que contém um tipo de ataque e suas informações */
+
+
