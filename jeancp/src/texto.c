7fcde5a6db698581e28f59803bae2b1ec1b4de58
Geroldor <geraldo.antonio@uel.br>
2022-10-06 14:31:18 -0300

    o mesmo texto só que menos duvidoso

--- jeancp/src/texto.c
+++ jeancp/src/texto.c
@@ -18,7 +18,7 @@ typedef struct texto
 
 Texto create_texto(int id, float x, float y, char *corb, char *corp, char *conteudo, char ancora)
 {
-    Stext *text = (Stext*)malloc(sizeof(Stext));
+    Stext *text = malloc(sizeof(Stext));
     text->id = id;
     text->x = x;
     text->y = y;
@@ -30,92 +30,92 @@ Texto create_texto(int id, float x, float y, char *corb, char *corp, char *conte
 }
 
 void texto_set_id(Texto text, int id){
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->id = id;
 
 }
 
 void texto_set_x(Texto text, float x)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->x = x;
 }
 
 void texto_set_y(Texto text, float y)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->y = y;
 }
 
 void texto_set_corb(Texto text, char *corb)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->corb = corb;
 }
 
 void texto_set_corp(Texto text, char *corp)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->corp = corp;
 }
 
 void texto_set_conteudo(Texto text, char *conteudo)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->conteudo = conteudo;
 }
 
 void texto_set_ancora(Texto text, char ancora)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     t->ancora = ancora;
 }
 
 int texto_get_id(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->id;
 }
 
 float texto_get_x(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->x;
 }
 
 float texto_get_y(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->y;
 }
 
 char *texto_get_corb(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->corb;
 }
 
 char *texto_get_corp(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->corp;
 }
 
 char *texto_get_conteudo(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->conteudo;
 }
 
 char texto_get_ancora(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     return t->ancora;
 }
 
 void texto_free(Texto text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = text;
     free(t->corb);
     free(t->corp);
     free(t->conteudo);
