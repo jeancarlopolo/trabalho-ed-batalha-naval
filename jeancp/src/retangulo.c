7b4528b85a231da77f7722d55e0257e6fcfe9393
Geroldor <geraldo.antonio@uel.br>
2022-10-06 14:24:30 -0300

    possivelmente um retangulo

--- jeancp/src/retangulo.c
+++ jeancp/src/retangulo.c
@@ -18,7 +18,7 @@ typedef struct retangulo{
 } Retangle;
 
 Retangulo create_retangulo(int id, float x, float y, float w, float h, string corb, string corp){
-    Retangle *ret = (Retangle*)malloc(sizeof(Retangle));
+    Retangle *ret = malloc(sizeof(Retangle));
     ret->id = id;
     ret->x = x;
     ret->y = y;
@@ -34,87 +34,87 @@ Retangulo create_retangulo(int id, float x, float y, float w, float h, string co
 }
 
 void retangulo_set_id(Retangulo ret, int id){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->id = id;
 }
 
 void retangulo_set_x(Retangulo ret, float x){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->x = x;
 }
 
 void retangulo_set_y(Retangulo ret, float y){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->y = y;
 }
 
 void retangulo_set_w(Retangulo ret, float w){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->w = w;
 }
 
 void retangulo_set_h(Retangulo ret, float h){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->h = h;
 }
 
 void retangulo_set_corb(Retangulo ret, string corb){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->corb = corb;
 }
 
 void retangulo_set_corp(Retangulo ret, string corp){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->corp = corp;
 }
 
 int retangulo_get_i(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->id;
 }
 
 float retangulo_get_x(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->x;
 }
 
 float retangulo_get_y(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->y;
 }
 
 float retangulo_get_w(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->w;
 }
 
 float retangulo_get_h(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->h;
 }
 
 string retangulo_get_corb(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->corb;
 }
 
 string retangulo_get_corp(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->corp;
 }
 
 float retangulo_get_area(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->area;
 }
 
 float retangulo_get_pontos_inativo(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     return r->pi;
 }
 
 float retangulo_get_pontos_destruido(Retangulo ret){
-    Retangle r = (Retangle*)ret;
+    Retangle *r = ret;
     r->pd = 90/(r->area/5);
     return r->pd;
 }
