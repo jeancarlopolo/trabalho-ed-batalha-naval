1c07863c2cb2f8cf2537ca1c9936b7ffbcfe34b2
tonhão mototaxi <jean.carlo.polo@uel.br>
2022-10-10 13:26:42 -0300

    tirando umas redundâncias do texto.c

--- jeancp/src/texto.c
+++ jeancp/src/texto.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "texto.h"
 
-typedef char *string;
 typedef struct texto
 {
     int id;
@@ -17,11 +13,11 @@ typedef struct texto
     int np;
     int pi;
     int pd;
-}Stext;
+} Stext;
 
 Barco create_texto(int id, float x, float y, char *corb, char *corp, char *conteudo, char ancora)
 {
-    Stext *text = (Stext*)malloc(sizeof(Stext));
+    Stext *text = (Stext *)malloc(sizeof(Stext));
     text->id = id;
     text->x = x;
     text->y = y;
@@ -36,107 +32,107 @@ Barco create_texto(int id, float x, float y, char *corb, char *corp, char *conte
     return text;
 }
 
-void texto_set_id(Barco text, int id){
-    Stext *t = (Stext*)text;
+void texto_set_id(Barco text, int id)
+{
+    Stext *t = (Stext *)text;
     t->id = id;
-
 }
 
 void texto_set_x(Barco text, float x)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->x = x;
 }
 
 void texto_set_y(Barco text, float y)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->y = y;
 }
 
 void texto_set_corb(Barco text, char *corb)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->corb = corb;
 }
 
 void texto_set_corp(Barco text, char *corp)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->corp = corp;
 }
 
 void texto_set_conteudo(Barco text, char *conteudo)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->conteudo = conteudo;
 }
 
 void texto_set_ancora(Barco text, char ancora)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     t->ancora = ancora;
 }
 
 int texto_get_id(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->id;
 }
 
 float texto_get_x(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->x;
 }
 
 float texto_get_y(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->y;
 }
 
 char *texto_get_corb(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->corb;
 }
 
 char *texto_get_corp(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->corp;
 }
 
 char *texto_get_conteudo(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->conteudo;
 }
 
 char texto_get_ancora(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     return t->ancora;
 }
 
-int texto_get_pontos_inativo(Barco text){
-    Stext *t = (Stext*)text;
+int texto_get_pontos_inativo(Barco text)
+{
+    Stext *t = (Stext *)text;
     return t->pi;
 }
 
-int texto_get_pontos_destruido(Barco text){
-    Stext *t = (Stext*)text;
+int texto_get_pontos_destruido(Barco text)
+{
+    Stext *t = (Stext *)text;
     return t->pd;
 }
 
 void texto_free(Barco text)
 {
-    Stext *t = (Stext*)text;
+    Stext *t = (Stext *)text;
     free(t->corb);
     free(t->corp);
     free(t->conteudo);
     free(t);
 }
-
-// Language: c
