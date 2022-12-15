8d0f83a85bb97d7a193c122c11fbe98eef21fecf
camilamaiadevalmeida <camila.maia.almeida@uel.br>
2022-12-15 14:43:31 -0300

    omain

--- jeancp/src/main.c
+++ jeancp/src/main.c
@@ -2,83 +2,28 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 4)
-    {
-        printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-        return 1;
-    }
+
     char *arqQry = (char *)malloc(sizeof(char) * 200);
     char *arqGeo = (char *)malloc(sizeof(char) * 200);
     char *dir = (char *)malloc(sizeof(char) * 200);
     char *path = (char *)malloc(sizeof(char) * 200);
-    if (strcmp(argv[1], "-e") == 0)
+    for (int i = 1; i < argc; i++)
     {
-        normalizePath(argv[2], path, strlen(argv[2]));
-        if (strcmp(argv[3], "-f") == 0)
+        if (strcmp(argv[i], "-e") == 0)
         {
-            strcpy(arqGeo, argv[4]);
-            if (strcmp(argv[5], "-q") == 0)
-            {
-                strcpy(arqQry, argv[6]);
-                if (strcmp(argv[7], "-o") == 0)
-                {
-                    normalizePath(argv[8], dir, strlen(argv[8]));
-                }
-                else
-                {
-                    printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-                    return 1;
-                }
-            }
-            else if (strcmp(argv[5], "-o") == 0)
-            {
-                normalizePath(argv[6], dir, strlen(argv[6]));
-            }
-            else
-            {
-                printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-                return 1;
-            }
+            normalizePath(argv[i + 1], path, strlen(argv[i + 1]));
         }
-        else
+        if (strcmp(argv[i], "-f") == 0)
         {
-            printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-            return 1;
+            strcpy(arqGeo, argv[i + 1]);
         }
-    }
-    else
-    {
-        path = ".";
-        if (strcmp(argv[1], "-f") == 0)
+        if (strcmp(argv[i], "-q") == 0)
         {
-            strcpy(arqGeo, argv[2]);
-            if (strcmp(argv[3], "-q") == 0)
-            {
-                strcpy(arqQry, argv[4]);
-                if (strcmp(argv[5], "-o") == 0)
-                {
-                    normalizePath(argv[6], dir, strlen(argv[6]));
-                }
-                else
-                {
-                    printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-                    return 1;
-                }
-            }
-            else if (strcmp(argv[3], "-o") == 0)
-            {
-                normalizePath(argv[4], dir, strlen(argv[4]));
-            }
-            else
-            {
-                printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-                return 1;
-            }
+            strcpy(arqQry, argv[i + 1]);
         }
-        else
+        if (strcmp(argv[i], "-o") == 0)
         {
-            printf("Uso: ted [-e path] -f arq.geo [-q consulta.qry] -o dir");
-            return 1;
+            normalizePath(argv[i + 1], dir, strlen(argv[i + 1]));
         }
     }
     Lista listabarcos = createLista(-1);
